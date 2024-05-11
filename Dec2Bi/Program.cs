0ddf694d4746a4c3fbe6d64b9bf90f15f0c3f0a0
posselinha <gabrielfilipeposs@hotmail.com>
2024-05-11 14:47:24 -0300

    Adicionar arquivos de projeto.

---
+++ Dec2Bi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+bool isValid = true;
+
+do
+{
+    Console.WriteLine("Digite o número que deseja converter para decimal:");
+    string? decimalInput = Console.ReadLine();
+    bool isValidParse = int.TryParse(decimalInput, out int parsedDecimal);
+
+    if(isValidParse)
+    {
+        string result = "";
+        int divisionResult = parsedDecimal;
+        while (divisionResult != 1)
+        {
+            int division = divisionResult / 2;
+            string remainer = Convert.ToString(divisionResult % 2);
+            
+            divisionResult = division;
+            result = remainer + result;
+            
+        }
+        Console.WriteLine("1" + result);
+    }
+    else
+    {
+        Console.WriteLine("Insira úm número decimal!");
+    }
+} while (isValid);
